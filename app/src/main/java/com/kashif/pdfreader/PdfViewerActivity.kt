d64dffa175e3eb4e72b51657e009a44e825c2dee
kashif <kashifqamar1729@gmail.com>
2025-12-23 16:05:38 +0530

    "v0.0.4 SAF pdf picker working"

--- app/src/main/java/com/kashif/pdfreader/PdfViewerActivity.kt
+++ app/src/main/java/com/kashif/pdfreader/PdfViewerActivity.kt
@@ -1,17 +1,30 @@
 package com.kashif.pdfreader
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kashif.pdfreader.ui.theme.PdfReaderTheme
 
 class PdfViewerActivity : ComponentActivity() {
@@ -30,10 +43,36 @@ class PdfViewerActivity : ComponentActivity() {
 }
 @Composable
 fun PdfViewerApp() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+
+    var selectedUri by remember { mutableStateOf<Uri?>(null) }
+
+    val openPdfLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.OpenDocument()
+        ) { uri ->
+            if (uri != null) {
+                selectedUri = uri
+            }
+        }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
     ) {
-        Text("PDF Viewer Base Ready")
+
+        Button(onClick = {
+            openPdfLauncher.launch(arrayOf("application/pdf"))
+        }) {
+            Text("Open PDF")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = selectedUri?.toString() ?: "No PDF selected",
+            fontSize = 12.sp
+        )
     }
 }
